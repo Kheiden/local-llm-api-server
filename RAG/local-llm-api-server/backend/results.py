b0ae0e688987602415eea1d523b391afc2c39e47
Kurt Heiden <kurt.w.heiden@gmail.com>
2024-02-17 19:54:12 -0700

    First attempt at importing FaissEmbeddingStorage from RTX install location

--- RAG/local-llm-api-server/backend/results.py
+++ RAG/local-llm-api-server/backend/results.py
@@ -1,9 +1,14 @@
+import os
 from typing import List
+from backend.utils import FaissEmbeddingStorage
 
 class Results:
 
   def __init__(self):
-    pass
+    data_dir = os.getenv('DATASET_DIR')
+    faiss_storage = FaissEmbeddingStorage(data_dir=data_dir,
+                                              dimension=os.getenv('DIMENSION_COUNT'))
+
 
   def returnTop3Matches(self, query)-> List[str]:
     # TODO: integrate with vector storage query
